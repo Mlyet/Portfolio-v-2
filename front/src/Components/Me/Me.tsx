5ab0796f01b14dd3e69762a6ad2533ec824b0c09
Thomas <thomas.meyet@epitech.eu>
2023-09-21 15:44:59 +0200

    ME Import the camera picture

--- front/src/Components/Me/Me.tsx
+++ front/src/Components/Me/Me.tsx
@@ -4,11 +4,11 @@ import gradient from '../../assets/angryimg.png';
 const Me = () => {
     return (
         <>
-            <div className="bg-white">
-                <img src={gradient} alt="" className='lap:mt-[60vw] w-[300vw] h-[20vw] ph:mt-[90vw]'/>
+            <div className="bg-white lap:mt-[60vw] w-[300vw] h-[20vw] ph:mt-[90vw]">
+                {/* <img src={gradient} alt="" className='lap:mt-[60vw] w-[300vw] h-[20vw] ph:mt-[90vw]'/> */}
                 <h1 className="text-center">Tho<span>m</span>as MEY<span>E</span>T</h1>
                 <div className='lap:flex'>
-                    <img src={portrait} alt="portrait" className='w-40' />
+                    {/* <img src={portrait} alt="portrait" className='w-40' /> */}
                     <p>Je suis étudiant à la web@cademie by Epitech</p>
                 </div>
             </div>
