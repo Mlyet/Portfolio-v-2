cfa53019ee7cac604d9f5f0096530478d5ba85f7
Thomas <thomas.meyet@epitech.eu>
2023-10-02 17:06:02 +0200

    DEV Fix the error with respect to page refresh at cursor style

--- front/src/Components/Cursor/Cursor.tsx
+++ front/src/Components/Cursor/Cursor.tsx
@@ -1,24 +1,27 @@
-import { Fragment} from "react";
+import { Fragment, useEffect} from "react";
 import "./Cursor.css";
 
 const Cursor = () => {
-    const dot = document.querySelector("[data-cursor-dot]") as HTMLElement | null;
-    const outline = document.querySelector("[data-cursor-outline]");
+    
+    useEffect (() => {
+        
+        const dot = document.querySelector("[data-cursor-dot]") as HTMLElement | null;
+        const outline = document.querySelector("[data-cursor-outline]");
 
-    window.addEventListener("mousemove", (event) => {
-        const postX = event.clientX;
-        const postY = event.clientY;
-
-        dot?.style && (dot.style.left = `${postX}px`);
-        dot?.style && (dot.style.top = `${postY}px`);
-
-        outline?.animate({
-            left: `${postX}px`,
-            top: `${postY}px`,
-        }, {duration: 5000, fill: "forwards"});
-
-
-    })
+        window.addEventListener("mousemove", (event) => {
+            const postX = event.clientX;
+            const postY = event.clientY;
+    
+            dot?.style && (dot.style.left = `${postX}px`);
+            dot?.style && (dot.style.top = `${postY}px`);
+    
+            outline?.animate({
+                left: `${postX}px`,
+                top: `${postY}px`,
+            }, {duration: 5000, fill: "forwards"});
+    
+        })
+    }, [])
     return (
         <Fragment>
             <div className="cursor-dot" data-cursor-dot></div>
